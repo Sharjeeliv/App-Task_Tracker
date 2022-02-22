0eac0aec1f3a6b41bdee5e26ac05d5b85efd67ce
Sharjeel Mustafa <sharjeeliv@gmail.com>
2022-02-21 20:08:20 -0500

    Complete todo app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import {useState} from "react";
 
 // Prop Types, kinda like a builtin type system for properties
 
 function App() {
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState(
         [
             {
@@ -27,6 +29,12 @@ function App() {
             }
         ]
     )
+    // Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = {id, ...task}
+        setTasks([...tasks, newTask])
+    }
 
     // Delete Task
     const deleteTask = (id) => {
@@ -40,7 +48,8 @@ function App() {
 
     return (
         <div className="container">
-            <Header/>
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+            {showAddTask && <AddTask onAdd={addTask}/>}
             {tasks.length > 0 ?
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to Show'}
         </div>
